10b835c5f5589579280fbdf3ee0abb8848786019
Assem <assem.yeskabyl@nu.edu.kz>
2020-05-22 23:22:28 +0600

    updated settings

--- elefanto_proj/settings.py
+++ elefanto_proj/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9jba+g%4ri-1aqyrrs$assifojf=!i(f&_c*%h184ke$acw4*p'
-
+# SECRET_KEY = '9jba+g%4ri-1aqyrrs$assifojf=!i(f&_c*%h184ke$acw4*p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['elefantoapp.herokuapp.com']
 
 
 # Application definition
